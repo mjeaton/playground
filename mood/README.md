ecaf939cec0a43dfbcb8740bf7a5569ffaa3d42a
Michael Eaton <mjeaton@gmail.com>
2018-04-30 23:40:47 -0400

    added README. Refactored.

---
+++ mood/README.md
@@ -0,0 +1,9 @@
+## Mood Diary
+
+This is my "learning Ruby for Real This Time" playground application. I use several applications (mostly mobile) to track health-related things like weight, sleep, water consumption, etc. Each has a way to enter "mood", but only once a day. I want something that allows me to track it throughout the day to help identify trends. I'm sure there are applications out there that do this, BUT, I want to learn Ruby and I do NOT want to write YATDL (yet another to do list).
+
+The goal is to start simple, using only what the base language provides, then, hopefully with some guidance from an experienced Ruby dev, grow it to do more.
+
+Plans:
+1. Get the basics working using a simple text file. This means Being able to add a new entry, list entries, clear all entries.
+2. Display a small report of mood over time.
--- mood/mood.rb
+++ mood/mood.rb
@@ -1,36 +1,76 @@
+class Diary
+	def SaveEntry(entry, delimiter="=")
+		begin
+			diary = File.open("moodDiary.txt", "a") 
+			diary.puts delimiter * 10
+			diary.puts entry.Timestamp 
+			diary.puts entry.Mood 
+			diary.puts entry.Reason 
+		rescue IOError => e
+			puts e
+		ensure
+			diary.close unless diary.nil?
+		end
+	end
+
+	def Clear
+		begin
+			diary = File.open("moodDiary.txt", "w") 
+		rescue IOError => e
+			puts e
+		ensure
+			diary.close unless diary.nil?
+		end
+	end
+
+	def GetMoodScale 
+		"Mood scale: 1=Blah, 2=Meh, 3=Ok, 4=Good, 5=Great"
+	end
+end
+
 class Mood
 	attr_accessor :Mood
 	attr_accessor :Reason
 	attr_accessor :Timestamp
+
+	def initialize(mood, reason, time)
+		@Mood = mood
+		@Reason = reason
+		@Timestamp = time
+	end
 end
 
-def writeEntry(diary, entry)
-	diary.puts "=" * 10
-	diary.puts entry.Timestamp 
-	diary.puts entry.Mood 
-	diary.puts entry.Reason 
+def Prompt
+	puts diary.GetMoodScale 
+	puts "What's your mood right now?"
+	currentMood = gets.chomp.to_i
+
+	puts "Why?"
+	currentMoodReason = gets.chomp
 end
 
-puts "Mood scale: 1=Blah, 2=Meh, 3=Ok, 4=Good, 5=Great"
-puts "What's your mood right now?"
-currentMood = gets.chomp.to_i
+diary = Diary.new
 
-puts "Why?"
-currentMoodReason = gets.chomp
+if ARGV.count == 0
+	Prompt
+elsif ARGV.count == 1
+	# look into an Options parser lib
+	if ARGV[0] == "/reset"
+		puts "Are you sure you want to clear the diary? (y/N)"
+		answer = gets.chomp
+
+		if(answer == "" || answer == 'N' || answer == 'n')
+			puts "Ok, NOT clearing the diary this time."
+		else
+			diary.Clear
+			exit(0)
+		end
+	end
+end
 
 puts "Current mood: #{currentMood}."
 puts "Reason: #{currentMoodReason}."
 
-entry = Mood.new
-entry.Mood = currentMood
-entry.Reason = currentMoodReason
-entry.Timestamp = Time.now
+entry = Mood.new currentMood, currentMoodReason, Time.now
 
-begin
-	moodDiary = File.open("moodDiary.txt", "a") 
-	writeEntry(moodDiary, entry)
-rescue IOError => e
-	puts e
-ensure
-	moodDiary.close unless moodDiary.nil?
-end
+diary.SaveEntry(entry)
