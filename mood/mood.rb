7e04f45a6e8aff5a167f586df6846601aca12078
Michael Eaton <mjeaton@gmail.com>
2018-05-02 08:16:25 -0400

    identifying areas for refactoring

--- mood/mood.rb
+++ mood/mood.rb
@@ -24,7 +24,7 @@ class Diary
 	end
 
 	def GetMoodScale 
-		"Mood scale: 1=Blah, 2=Meh, 3=Ok, 4=Good, 5=Great"
+		"Mood scale: 1=Terrible, 2=Bad, 3=Okay, 4=Good, 5=Great"
 	end
 end
 
@@ -43,6 +43,8 @@ end
 diary = Diary.new
 
 if ARGV.count == 0
+	# wrap this in a Prompt method that can be called from 
+	# other places.
 	puts diary.GetMoodScale 
 	puts "What's your mood right now?"
 	currentMood = gets.chomp.to_i
@@ -51,6 +53,10 @@ if ARGV.count == 0
 	currentMoodReason = gets.chomp
 elsif ARGV.count == 1
 
+	# some of this feels dirty and not very Ruby-like
+	# need to figure out a better way to to handle the answer (I have some ideas...)
+	# also, the multiple exits(0) really bother me.1
+
 	# look into an Options parser lib
 	if ARGV[0] == "--reset"
 		puts "Are you sure you want to clear the diary? (y/N)"
@@ -58,17 +64,19 @@ elsif ARGV.count == 1
 
 		if(answer == "" || answer == 'N' || answer == 'n')
 			puts "Ok, NOT clearing the diary this time."
-			exit(0)	
 		else
 			diary.Clear
 			puts "Diary has been cleared."
-			exit(0)
 		end
+		exit(0)
+	else
+		puts "I don't know what you want to do. Exiting."
+		exit(0)
 	end
 end
 
-puts "Current mood: #{currentMood}."
-puts "Reason: #{currentMoodReason}."
+# fix so it shows the mapped currentMood instead of the number (git issue #2)
+puts "You are currently in a #{currentMood} mood because '#{currentMoodReason}'."
 
 entry = Mood.new currentMood, currentMoodReason, Time.now
 
