157abd71437abb2e041e764c00fae41ac0be31cc
Michael Eaton <mjeaton@gmail.com>
2018-05-04 15:24:28 -0400

    pulled multi-line for now

--- mood/mood.rb
+++ mood/mood.rb
@@ -19,9 +19,10 @@ end
 
 def ask_for_reason 
 	puts "Why? (shift-enter for multiple lines. 'END' <enter> to save."
+	gets.chomp
 
-	$/ = "END"
-	STDIN.gets
+	#$/ = "END"
+	#STDIN.gets
 end
 
 def prompt_and_get_entry(diary)
